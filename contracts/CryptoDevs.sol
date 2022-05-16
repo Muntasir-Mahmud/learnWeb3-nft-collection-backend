d8c2348d3b06016054565f3d4a849777d749dfa6
Muntasir-Mahmud <m.siyam529@gmail.com>
2022-05-16 19:05:28 +0600

    Added other function of SC

--- contracts/CryptoDevs.sol
+++ contracts/CryptoDevs.sol
@@ -47,4 +47,19 @@ contract CryptoDevs is ERC721Enumerable, Ownable {
         tokenIds += 1;
         _safeMint(msg.sender, tokenIds);
     } 
+
+    function setPaused(bool val) public onlyOwner {
+        _paused = val;
+    }
+
+    function withdraw() public onlyOwner {
+        address _owner = owner();
+        uint256 amount = address(this).balance;
+        (bool sent,) = _owner.call{value: amount}("");
+        require(sent, "Failed to send Ether");
+    }
+
+    receive() external payable {}
+
+    fallback() external payable {}
 }
