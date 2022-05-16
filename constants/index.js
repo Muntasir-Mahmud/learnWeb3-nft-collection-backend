b677a13f9eaee0635aea153064d2cdc9b7cb1fee
Muntasir-Mahmud <m.siyam529@gmail.com>
2022-05-16 23:41:45 +0600

    added deploy.js

---
+++ constants/index.js
@@ -0,0 +1,4 @@
+const WHITELIST_CONTRACT_ADDRESS = "0x3b773f22c81f78E83FBce2d36f3CeCdA29466213";
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
---
+++ scripts/deploy.js
@@ -0,0 +1,28 @@
+require("dotenv").config({ path: ".env" });
+const { ethers } = require("hardhat");
+const { WHITELIST_CONTRACT_ADDRESS, METADATA_URL } = require("../constants");
+
+async function main() {
+    const whitelistContract = WHITELIST_CONTRACT_ADDRESS;
+    const metadataURL = METADATA_URL;
+
+    const cryptoDevsContract = await ethers.getContractFactory("CryptoDevs");
+
+    const deployedCryptoDevsContract = await cryptoDevsContract.deploy(
+        metadataURL,
+        whitelistContract
+    );
+
+    console.log(
+        "CryptoDevs Contract address: ",
+        deployedCryptoDevsContract.address
+    );
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+
